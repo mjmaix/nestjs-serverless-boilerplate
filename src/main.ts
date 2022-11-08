004da8f91462f7dbe810205581cc2148f7b325e5
MJ Abadilla <mjmaix@gmail.com>
2022-11-08 23:23:09 +0800

    Remove webpack - cannot make esnext work

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.time('startup time');
   const app = await NestFactory.create(AppModule);
   const port = env.PORT || 3000;
   await app.listen(port);
   console.log(`Now listening on port ${port}`);
+  console.timeEnd('startup time');
 }
 
 bootstrap();
